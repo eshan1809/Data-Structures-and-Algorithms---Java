2eabb8faccd4e436ed91f104b1210aa4534e1944
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2020-10-01 10:31:27 +0530

    Create Search in 2D Array.java

---
+++ Basics of programming/2D Array/Search in 2D Array.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[][] a = new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                a[i][j] = scn.nextInt();
+            }
+        }
+        int f = scn.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(a[i][j]==f){
+                    System.out.print(i + "\n" + j);
+                    return;
+                }
+            }
+        }
+        System.out.print("Not Found");
+    }
+}
