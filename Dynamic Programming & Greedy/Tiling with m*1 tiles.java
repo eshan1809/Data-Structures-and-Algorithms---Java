408dd6d45ff0d6db554ac35770854cb1df2bfc34
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2020-10-07 07:12:08 +0530

    Create Tiling with m*1 tiles.java

---
+++ Dynamic Programming & Greedy/Tiling with m*1 tiles.java
@@ -0,0 +1,31 @@
+/*
+1. You are given a number n and a number m separated by line-break representing the length and breadth of a n * m floor.
+2. You've an infinite supply of m * 1 tiles.
+3. You are required to calculate and print the number of ways floor can be tiled using tiles.
+*/
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int m = scn.nextInt();
+    int[] dp = new int[n];
+    for(int i=0;i<n;i++){
+        if(i<m-1){
+            dp[i]=1;
+        }
+        else if(i==m-1){
+            dp[i]=2;
+        }
+        else{
+            dp[i]=dp[i-1]+dp[i-m];
+        }
+    }
+    System.out.print(dp[n-1]);
+}
+}
