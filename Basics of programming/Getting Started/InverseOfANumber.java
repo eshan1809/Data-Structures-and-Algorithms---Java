3c66a33c6145597a9849aae772a08c8256169528
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2021-10-16 15:57:36 +0530

    Create InverseOfANumber.java

---
+++ Basics of programming/Getting Started/InverseOfANumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        int n = str.length();
+        int[] arr = new int[n + 1];
+        for(int i = 0; i < n; i++)
+            arr[str.charAt(i) - '0'] = i + 1;
+        for(int i = 1; i <= n; i++)
+            System.out.print(arr[i]);
+    }
+}
